11fa424477e62965807e065ef005f9c1d4c9e6c3
Fathima nasmin <fathimanesmi@gmail.com>
2023-08-17 12:53:16 +0530

    added search feature

--- lms_main/urls.py
+++ lms_main/urls.py
@@ -7,8 +7,11 @@ app_name = 'lms_main'
 
 urlpatterns = [
     path('', lms_main_view.home_page, name='home'),
-    path('category-detail/<int:id>/', lms_main_view.category_detail, name='category-detail'),
+    path('search/', lms_main_view.search, name='search'),
+    path('category-detail/<int:id>/',
+         lms_main_view.category_detail, name='category-detail'),
     path('course-lists/', lms_main_view.course_lists, name='course-lists'),
-    path('single-course/<int:id>/', lms_main_view.single_course, name='single-course'),
-    
+    path('single-course/<int:id>/',
+         lms_main_view.single_course, name='single-course'),
+
 ]
--- lms_main/views.py
+++ lms_main/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 from . import models
+
 
 def home_page(request):
     categories = models.Category.objects.all()
     courses = models.Course.objects.filter(status='PUBLISH').all()
-    print(courses)
-    print(categories)
+    # print(courses)
+    # print(categories)
 
     context = {
         'categories': categories,
@@ -16,24 +17,38 @@ def home_page(request):
     return render(request, 'lms_main/home.html', context)
 
 
+def search(request):
+    query = request.GET.get('query')
+    search_results = models.Course.objects.filter(title__icontains=query).all()
+    # print(courses)
+
+    context = {
+        'search_results': search_results,
+    }
+
+    return render(request, 'lms_main/search.html', context)
+
+    # return JsonResponse({"message": "message for search view",
+    #                      'query_result': courses})
+
 
 def category_detail(request, id):
     """displays all the courses under the category"""
     category_obj = models.Category.objects.get(id=id)
     categories = models.Category.objects.all()
-    category_courses = models.Course.objects.filter(status='PUBLISH',category_id=category_obj.id).all()
+    category_courses = models.Course.objects.filter(
+        status='PUBLISH', category_id=category_obj.id).all()
 
     # print(category_courses)
     # print(category_obj)
 
     context = {
         'categories': categories,
-        'category_courses':category_courses,
-        'category_obj':category_obj,
+        'category_courses': category_courses,
+        'category_obj': category_obj,
     }
     # for course in category_courses:
     #     print(course.author.instructor.last_name)
-        
 
     return render(request, 'lms_main/category_detail.html', context)
 
@@ -48,7 +63,6 @@ def course_lists(request):
     }
     # for course in all_courses:
     #     print(course.author.instructor.first_name)
-        
 
     return render(request, 'lms_main/course_lists.html', context)
 
@@ -56,11 +70,9 @@ def course_lists(request):
 def single_course(request, id):
     """view for single course in detail"""
     single_course = models.Course.objects.get(id=id)
-    print(single_course)
+    # print(single_course)
     context = {
         'course': single_course,
     }
 
     return render(request, 'lms_main/single_course.html', context)
-
-
