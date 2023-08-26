e5e3468df12ca1aaed45a4eb61b4c6a96c44cdf5
Fathima nasmin <fathimanesmi@gmail.com>
2023-08-26 11:15:20 +0530

    changes in settings.py

--- lms_app/settings.py
+++ lms_app/settings.py
@@ -32,6 +32,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'crispy_forms',
     "crispy_bootstrap5",
+    # paypalintegration
+    'paypal.standard.ipn',
 
     "user",
     "lms_main",
